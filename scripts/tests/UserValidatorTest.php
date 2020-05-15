87121dde7b3126c3d9f9747f522e58ace837afa4
quinnjoplinMH <58871866+quinnjoplinMH@users.noreply.github.com>
2020-05-15 04:50:14 -0400

    Add Tests (#4)
    * Add files via upload
    
    * Rename UserValidatorTest.php to scripts/tests/UserValidatorTest.php

---
+++ scripts/tests/UserValidatorTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class UserValidatorTest extends TestCase {
+    public function testFetchRecords() {
+        
+    }
+    
+    public function testValidateRecords () {
+        
+    }
+}
