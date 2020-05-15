a7423cb293613ddf5467d994ea7a516461609256
quinnjoplinMH <58871866+quinnjoplinMH@users.noreply.github.com>
2020-05-15 04:58:12 -0400

    Update UserValidatorTest.php

--- scripts/tests/UserValidatorTest.php
+++ scripts/tests/UserValidatorTest.php
@@ -3,10 +3,10 @@ use PHPUnit\Framework\TestCase;
 
 class UserValidatorTest extends TestCase {
     public function testFetchRecords() {
-        
+        $this->assertTrue(false);
     }
     
     public function testValidateRecords () {
-        
+        $this->assertTrue(false);
     }
 }
